8a481911c0c39fe235276c0ab5d17de7bcccd965
Serge Vorobyov <svserge89@gmail.com>
2021-03-18 22:02:47 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import './config/dotenv.config';
+
+import {Logger} from '@nestjs/common';
+import {NestFactory} from '@nestjs/core';
+import {get as getConfig} from 'config';
+
+import {AppModule} from './app.module';
+import {ServerConfig} from './config/server-config.interface';
+
+const DEFAULT_PORT = getConfig<ServerConfig>('server').port;
+
+async function bootstrap() {
+  const logger = new Logger('main');
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT
+    ? Number.parseInt(process.env.PORT)
+    : DEFAULT_PORT;
+
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
+}
+bootstrap();
