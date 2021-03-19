2063cef90634b11d1f00cb21a1d308941ca1561e
Serge Vorobyov <svserge89@gmail.com>
2021-03-19 11:48:33 +0300

    Add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import {Module} from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 
 import {typeOrmConfig} from './config/typeorm.config';
+import {UserModule} from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
-  controllers: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
   providers: [],
 })
 export class AppModule {}
