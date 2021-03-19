95488119185a2fdda2b756d01349db19b90922d2
Serge Vorobyov <svserge89@gmail.com>
2021-03-19 11:28:58 +0300

    Configure database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+
+import {typeOrmConfig} from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [],
   providers: [],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,20 @@
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+import {get as getConfig} from 'config';
+
+import {DatabaseConfig} from './database-config.interface';
+
+const config = getConfig<DatabaseConfig>('database');
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST ?? config.host,
+  port: process.env.DATABASE_PORT
+    ? Number.parseInt(process.env.DATABASE_PORT)
+    : config.port,
+  database: process.env.DATABASE_NAME ?? config.database,
+  username: process.env.DATABASE_USERNAME ?? config.username,
+  password: process.env.DATABASE_PASSWORD ?? config.password,
+  autoLoadEntities: true,
+  synchronize: false,
+  logging: config.logging,
+};
