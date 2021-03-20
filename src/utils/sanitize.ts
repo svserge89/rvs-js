c259de8b979b693c2be06ceca5c272578386a303
Serge Vorobyov <svserge89@gmail.com>
2021-03-20 17:29:01 +0300

    Add sanitize functions

---
+++ src/utils/sanitize.ts
@@ -0,0 +1,31 @@
+import {TransformFnParams} from 'class-transformer';
+
+export function trim({value}: TransformFnParams) {
+  if (typeof value === 'string') {
+    return value.trim();
+  }
+
+  return value;
+}
+
+export function split({value}: TransformFnParams) {
+  if (typeof value === 'string') {
+    return value.split(',').map((item) => item.trim());
+  }
+
+  return value;
+}
+
+export function toBoolean({value}: TransformFnParams) {
+  if (typeof value === 'string') {
+    if (value.trim().toLowerCase() === 'true') {
+      return true;
+    }
+
+    if (value.trim().toLowerCase() === 'false') {
+      return false;
+    }
+  }
+
+  return value;
+}
