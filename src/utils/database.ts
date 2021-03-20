cefb59365df5bba68466d42feedc97a05cee66f6
Serge Vorobyov <svserge89@gmail.com>
2021-03-20 17:35:21 +0300

    Add database utils

---
+++ src/utils/database.ts
@@ -0,0 +1,6 @@
+export const NAME_LENGTH = 100;
+export const EMAIL_LENGTH = 100;
+export const URL_LENGTH = 100;
+export const ENCRYPTED_PASSWORD_LENGTH = 60;
+export const UNIQUE_VIOLATION = '23505';
+export const CHECK_VIOLATION = '23514';
