ac2292cb236d463a14d45ec58dc139341b7d5cc5
Serge Vorobyov <svserge89@gmail.com>
2021-03-23 21:02:03 +0300

    Add restaurant module stubs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 
 import {AuthModule} from './auth/auth.module';
 import {typeOrmConfig} from './config/typeorm.config';
+import {RestaurantModule} from './restaurant/restaurant.module';
 import {UserModule} from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    UserModule,
+    AuthModule,
+    RestaurantModule,
+  ],
   providers: [],
 })
 export class AppModule {}
