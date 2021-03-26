c86898428e7ac0632e066f09e45a55156b813c39
Serge Vorobyov <svserge89@gmail.com>
2021-03-27 01:22:45 +0300

    Implement dish service

--- src/restaurant/dish/dish.module.ts
+++ src/restaurant/dish/dish.module.ts
@@ -1,9 +1,13 @@
 import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
+import {AuthModule} from '../../auth/auth.module';
 import {DishController} from './dish.controller';
 import {DishService} from './dish.service';
+import {DishEntity} from './entity/dish.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DishEntity]), AuthModule],
   providers: [DishService],
   controllers: [DishController],
 })
--- src/restaurant/dish/dish.service.ts
+++ src/restaurant/dish/dish.service.ts
@@ -1,4 +1,196 @@
-import {Injectable} from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import {InjectRepository} from '@nestjs/typeorm';
+import {Repository} from 'typeorm';
+
+import {AllFieldsIsEmptyException} from '../../exception/all-fields-is-empty.exception';
+import {UnknownException} from '../../exception/unknown.exception';
+import {
+  CHECK_VIOLATION,
+  createFindQueryBuilder,
+  UNIQUE_VIOLATION,
+} from '../../utils/database';
+import {RestaurantEntity} from '../entity/restaurant.entity';
+import {RestaurantNotFoundException} from '../exception/restaurant-not-found.exception';
+import {CreateDishDto} from './dto/create-dish.dto';
+import {
+  DishPageResponseDto,
+  toDishPageResponseDto,
+} from './dto/dish-page-response.dto';
+import {DishResponseDto, toDishResponseDto} from './dto/dish-response.dto';
+import {FindDishesDto} from './dto/find-dishes.dto';
+import {UpdateDishDto} from './dto/update-dish.dto';
+import {DishEntity} from './entity/dish.entity';
+import {DishConflictNameException} from './exception/dish-conflict-name.exception';
+import {DishInvalidNameException} from './exception/dish-invalid-name.exception';
+import {DishNotFoundException} from './exception/dish-not-found.exception';
+
+const DEFAULT_FILTER_FIELDS: (keyof DishEntity)[] = ['name', 'description'];
 
 @Injectable()
-export class DishService {}
+export class DishService {
+  private readonly logger = new Logger('DishService');
+
+  constructor(
+    @InjectRepository(DishEntity)
+    private readonly dishRepository: Repository<DishEntity>,
+  ) {}
+
+  async create(
+    restaurantId: string,
+    {name, description, date}: CreateDishDto,
+  ): Promise<DishResponseDto> {
+    try {
+      return toDishResponseDto(
+        await this.dishRepository.manager.transaction(async (tm) => {
+          const restaurant = await tm.findOne(RestaurantEntity, restaurantId, {
+            select: ['id'],
+          });
+
+          if (!restaurant) {
+            throw new RestaurantNotFoundException(restaurantId);
+          }
+
+          const dish = this.dishRepository.create({
+            name,
+            description,
+            date,
+            restaurant,
+          });
+
+          return await tm.save(dish);
+        }),
+      );
+    } catch (exception) {
+      this.checkException(exception, name);
+    }
+  }
+
+  async update(
+    restaurantId: string,
+    id: string,
+    {name, description, date}: UpdateDishDto,
+  ): Promise<DishResponseDto> {
+    if (!name && !description && !date) {
+      throw new AllFieldsIsEmptyException();
+    }
+
+    try {
+      return toDishResponseDto(
+        await this.dishRepository.manager.transaction(async (tm) => {
+          const updateFields: Partial<DishEntity> = {};
+
+          if (name) {
+            updateFields.name = name;
+          }
+
+          if (description) {
+            updateFields.description = description;
+          }
+
+          if (date) {
+            updateFields.date = date;
+          }
+
+          const result = await tm.update(
+            DishEntity,
+            {id, restaurant: {id: restaurantId}},
+            updateFields,
+          );
+
+          if (!result.affected) {
+            throw new DishNotFoundException(id);
+          }
+
+          return await tm.findOne(DishEntity, {
+            id,
+            restaurant: {id: restaurantId},
+          });
+        }),
+      );
+    } catch (exception) {
+      this.checkException(exception, name);
+    }
+  }
+
+  async delete(restaurantId: string, id: string): Promise<void> {
+    try {
+      return await this.dishRepository.manager.transaction(async (tm) => {
+        const result = await tm.delete(DishEntity, {
+          id,
+          restaurant: {id: restaurantId},
+        });
+
+        if (!result.affected) {
+          throw new DishNotFoundException(id);
+        }
+      });
+    } catch (exception) {
+      this.checkException(exception);
+    }
+  }
+
+  async findOne(restaurantId: string, id: string): Promise<DishResponseDto> {
+    try {
+      const dish = await this.dishRepository.findOne({
+        id,
+        restaurant: {id: restaurantId},
+      });
+
+      if (!dish) {
+        throw new DishNotFoundException(id);
+      }
+
+      return toDishResponseDto(dish);
+    } catch (exception) {
+      this.checkException(exception);
+    }
+  }
+
+  async find(
+    restaurantId: string,
+    {page, size, sort, filter, filterFields}: FindDishesDto,
+  ): Promise<DishPageResponseDto> {
+    const queryBuilder = createFindQueryBuilder(this.dishRepository, {
+      page,
+      size,
+      sort,
+      filter,
+      filterFields: filterFields || DEFAULT_FILTER_FIELDS,
+    });
+
+    try {
+      const [dishes, total] = await queryBuilder.getManyAndCount();
+
+      return toDishPageResponseDto(dishes, page, size, total);
+    } catch (exception) {
+      this.checkException(exception);
+    }
+  }
+
+  private checkException(exception: any, name?: string) {
+    if (
+      exception instanceof NotFoundException ||
+      exception instanceof ConflictException
+    ) {
+      throw exception;
+    } else if (
+      exception.code === UNIQUE_VIOLATION ||
+      exception.code === CHECK_VIOLATION
+    ) {
+      this.logger.debug('Exception: ' + JSON.stringify(exception));
+
+      if (exception.constraint === 'dish_name_date_restaurant_id_key') {
+        throw new DishConflictNameException(name);
+      } else if (exception.constraint === 'dish_name_check') {
+        throw new DishInvalidNameException(name);
+      }
+    }
+
+    throw new UnknownException(this.logger, exception);
+  }
+}
